33dc0926c96682deab8e6ca96f3099f0c550af86
krmsxo <76913945+krmsxo@users.noreply.github.com>
2021-01-10 19:06:23 +0900

    1.2c

---
+++ 김근태/1.2c.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0, b = 0;
+	printf("점수를 입력하시오:");
+	scanf("%d", &a);
+	if (80 = < a = < 100)
+	{
+		printf("score:A");
+
+	}
+	else if (60 = < a < 80)
+	{
+
+		printf("score :B");
+	}
+
+}
