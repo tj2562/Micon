33ce36e8a10ef81f70b45886b65f3e3c89d363e5
kuksaerom <76939304+kuksaerom@users.noreply.github.com>
2021-01-12 01:49:41 +0900

    Create 1_1.c

---
+++ 국새롬/1_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float x, y;
+
+	printf("두 개의 숫자를 입력하세요: ");
+	scanf_s("%f %f", &x, &y);
+
+	printf("%f + %f = %f\n", x, y, x + y);
+	printf("%f - %f = %f\n", x, y, x - y);
+	printf("%f * %f = %f\n", x, y, x * y);
+	printf("%f / %f = %f\n", x, y, x / y);
+
+	return 0;
+}
