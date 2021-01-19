ea748d8f8be803b2f2ca81c429c4c96e06225d6c
kimgy97 <77225913+kimgy97@users.noreply.github.com>
2021-01-19 14:01:51 +0900

    Create Light.ino

---
+++ 김지영/Light/Light.ino
@@ -0,0 +1,35 @@
+
+int green=12;
+int yellow=11;
+int red=10;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(green, OUTPUT);
+pinMode(yellow, OUTPUT);
+pinMode(red, OUTPUT);
+pinMode(7, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+bool button = digitalRead(7);
+if(button == true) {
+    digitalWrite(green, LOW);
+    delay(600);
+    digitalWrite(yellow, LOW);
+    delay(600);
+    digitalWrite(red, LOW);
+    delay(600);
+    }
+        else{
+          digitalWrite(green,HIGH);
+          delay(600);
+          digitalWrite(yellow,HIGH);
+          delay(600);
+          digitalWrite(red,HIGH);
+          delay(600);
+          }
+      
+}
