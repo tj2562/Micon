5911b1ada6cfa00525aed4dee8a0a738645ca384
tlgus927 <sihyun927@naver.com>
2021-01-19 17:47:30 +0900

    과제2
    성공못했는데 아직 왜 그런지 모르겠어서 알아보는중입니다

---
+++ 김시현/2차과제(0115)/ThreeLED/ThreeLED.ino
@@ -0,0 +1,31 @@
+int green=13;
+int yellow=12;
+int red=11;
+int push=2;
+
+void setup() {
+  pinMode(push, INPUT_PULLUP);
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(red, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(green,LOW);
+  digitalWrite(yellow,LOW);
+  digitalWrite(red,LOW);
+  
+  int sw = digitalRead(push);
+  
+  if(sw==LOW){
+    digitalWrite(green,HIGH);
+    delay(600);
+    digitalWrite(green,LOW); 
+    digitalWrite(yellow,HIGH); //yellow
+    delay(600);
+    digitalWrite(yellow,LOW);
+    digitalWrite(red,HIGH); //red
+    delay(600);
+    digitalWrite(red,LOW);
+  }
+  }
