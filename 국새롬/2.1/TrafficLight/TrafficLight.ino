0e3b687d86d0c89bf80dd324b8f4855dfb582e40
tj2562 <70683231+tj2562@users.noreply.github.com>
2021-01-23 21:21:41 +0900

    Update TrafficLight.ino

--- 국새롬/2.1/TrafficLight/TrafficLight.ino
+++ 국새롬/2.1/TrafficLight/TrafficLight.ino
@@ -1,30 +1,24 @@
-//#define pushBtn 2
+#include <Servo.h>
+
+Servo myServo;
+int potPin = A0;
+int potVal;
+int angle;
 
 void setup() {
-  Serial.begin(9600); // 직렬통신 설정
- 
-  //pinMode(pushBtn,INPUT_PULLUP); 위에서 상수로 선언하여 쓰면 가독성 향상!!
-  pinMode(2,INPUT_PULLUP); // 2번을 입력으로
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
+  myServo.attach(5);
+  Serial.begin(9600);
 }
 
-
 void loop() {
-
-  int sw=digitalRead(2);
-  if (!sw){
-    digitalWrite(13,HIGH);
-    delay(600);
-    digitalWrite(13,LOW);
-    digitalWrite(12,HIGH);
-    delay(600);
-    digitalWrite(12,LOW);
-    digitalWrite(13,HIGH);
-    delay(600);
-    digitalWrite(13,LOW);
-    delay(600);
-    }
-
-    //good
+  potVal = analogRead(potPin);
+  Serial.print("potVal: ");
+  Serial.print(potVal);
+  
+  angle = map(potVal, 0, 1023, 0, 179);
+  Serial.print(", angle: ");
+  Serial.println(angle);
+  
+  myServo.write(angle);
+  delay(15);
 }
